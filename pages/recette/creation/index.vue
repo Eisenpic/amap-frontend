aca555179828e261b27eeaacb5baaf0a15fa2e29
Calvin Lambert <calvin.lambert@protonmail.com>
2022-02-09 17:22:55 +0100

    :sparkles: Fix bug & improve page of creation

--- pages/recette/creation/index.vue
+++ pages/recette/creation/index.vue
@@ -9,28 +9,28 @@
         étape.
       </h5>
       <b-field label="Nom de la recette">
-        <b-input name="nomrecette" expanded />
+        <b-input v-model="nomrecette" name="nomrecette" expanded />
       </b-field>
 
       <div class="is-flex is-flex-wrap-wrap is-justify-content-space-between">
         <b-field label="Saison">
-          <b-select placeholder="Choisir une saison">
-            <option value="1">
+          <b-select v-model="saison" placeholder="Choisir une saison">
+            <option value="Été">
               Été
             </option>
-            <option value="2">
+            <option value="Automne">
               Automne
             </option>
-            <option value="3">
+            <option value="Printemps">
               Printemps
             </option>
-            <option value="4">
+            <option value="Hiver">
               Hiver
             </option>
           </b-select>
         </b-field>
         <b-field label="Difficultée">
-          <b-select placeholder="Choisir une difficultée">
+          <b-select v-model="difficulte" placeholder="Choisir une difficultée">
             <option value="1">
               Facile
             </option>
@@ -43,20 +43,20 @@
           </b-select>
         </b-field>
         <b-field label="Régime">
-          <b-select placeholder="Choisir un régime">
-            <option value="1">
+          <b-select v-model="regime" placeholder="Choisir un régime">
+            <option value="Normal">
               Normal
             </option>
-            <option value="2">
+            <option value="Végétarien">
               Végétarien
             </option>
-            <option value="3">
+            <option value="Végan">
               Végan
             </option>
           </b-select>
         </b-field>
         <b-field label="Nombre de personnes">
-          <b-select placeholder="Choisir le nombre de personne">
+          <b-select v-model="nbpers" placeholder="Choisir le nombre de personne">
             <option value="1">
               1
             </option>
@@ -88,13 +88,33 @@
         Liste des ingrédients :
       </h5>
       <div class="columns is-multiline">
-        <div v-for="n in parseInt(nbing)" :key="n" class="mt-2 column is-one-quarter is-full-mobile is-full-tablet">
+        <b-input v-model="listing[0]" class="mt-2 column is-one-quarter is-full-mobile is-full-tablet" />
+        <div v-for="n in parseInt(nbing-1)" :key="n" class="mt-2 column is-one-quarter is-full-mobile is-full-tablet">
           <b-input v-model="listing[n]" />
         </div>
       </div>
 
-      <b-field label="Nombre d'étapes de la recette">
+      <b-field label="Nombres d'ustensiles">
+        <b-numberinput v-model="nbust" placeholder="5" min="1" max="5" />
+      </b-field>
+      <h5 class="subtitle is-5">
+        Liste des ustensiles :
+      </h5>
+      <div class="columns is-multiline">
+        <b-input v-model="listust[0]" class="mt-2 column is-one-quarter is-full-mobile is-full-tablet" />
+        <div v-for="n in parseInt(nbust-1)" :key="n" class="mt-2 column is-one-quarter is-full-mobile is-full-tablet">
+          <b-input v-model="listust[n]" />
+        </div>
+      </div>
+
+      <b-field label="Nombres d'étapes de la recette">
         <b-numberinput v-model="nbetapes" placeholder="5" min="1" max="15" />
+      </b-field>
+      <h5 class="title is-5">
+        Etape : {{ 0 }}
+      </h5>
+      <b-field>
+        <b-input v-model="descetape[0]" type="textarea" />
       </b-field>
       <div v-for="n in parseInt(nbetapes)" :key="n" class="mt-3">
         <h5 class="title is-5">
@@ -104,6 +124,7 @@
           <b-input v-model="descetape[n]" type="textarea" />
         </b-field>
       </div>
+      {{ descetape }}
     </section>
     <b-button expanded class="button is-primary">
       Envoyer la recette
@@ -117,10 +138,17 @@ export default {
   middleware: 'auth',
   data () {
     return {
+      nomrecette: '',
+      difficulte: 1,
+      saison: 'Été',
+      regime: 'Normal',
+      nbpers: 1,
       nbing: 1,
       nbetapes: 1,
+      nbust: 1,
       descetape: [],
-      listing: []
+      listing: [],
+      listust: []
     }
   }
 }
