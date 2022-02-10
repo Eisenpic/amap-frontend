64c6c3039e00a57b04ed2cec29cdb391d1e462ca
Lucas Humbert <lucas.humbert.28@gmail.com>
2022-02-10 11:01:10 +0100

    :sparkles: add contributeur recettes

--- pages/contributeurs/recettes/_id.vue
+++ pages/contributeurs/recettes/_id.vue
@@ -3,6 +3,18 @@
     <div class="has-text-centered title is-4">
       Recettes de {{ fullname }}
     </div>
+    <div>
+      <p>Top recette</p>
+      <div v-if="recettes.topRecette">
+        {{ recettes.topRecette }}
+      </div>
+    </div>
+    <div>
+      <p>Autres recettes</p>
+      <div v-for="recette in recettes.recettes" :key="recette.id">
+          {{ recette }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,7 +24,8 @@ export default {
   data () {
     return {
       ready: false,
-      contributeur: []
+      contributeur: [],
+      recettes: []
     }
   },
   computed: {
@@ -24,7 +37,17 @@ export default {
     this.$axios.get(`/api/contributeur/${this.$route.params.id}`)
       .then((response) => {
         this.contributeur = response.data
-        this.ready = true
+        this.$axios.get(`/api/contributeur/${this.$route.params.id}/recettes`)
+          .then((response) => {
+            this.recettes = response.data
+            this.ready = true
+          })
+          .catch(() => {
+            console.log('Erreur lors de la récupération des recettes')
+          })
+      })
+      .catch(() => {
+        console.log('Erreur lors de la récupération du contributeur')
       })
   }
 }
