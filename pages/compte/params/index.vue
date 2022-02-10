cbaef68f81aa0ef00941c36b0dc4da8a7cd0a988
Geoffrey <geoffrey.poraykostexp7@gmail.com>
2022-02-10 17:34:18 +0100

    filtre (saison régime difficulté et produits)

--- pages/compte/params/index.vue
+++ pages/compte/params/index.vue
@@ -11,23 +11,27 @@
         <div class="column is-3 box p-2 has-background-grey-lighter">
           <b-menu>
             <b-menu-list label="Paramètres">
-              <b-menu-item v-if="path !== 'default'" @click="moove('default')" icon="information-outline" label="Informations Personnelles" />
+              <b-menu-item v-if="path !== 'default'" icon="information-outline" label="Informations Personnelles" @click="moove('default')" />
               <b-menu-item v-else icon="information-outline" label="Informations Personnelles" disabled />
-              <b-menu-item v-if="path !== 'expertises'" @click="moove('expertises')" icon="star" label="Mes Expertises" />
+              <b-menu-item v-if="path !== 'expertises'" icon="star" label="Mes Expertises" @click="moove('expertises')" />
               <b-menu-item v-else icon="star" label="Mes Expertises" disabled />
-              <b-menu-item v-if="path !== 'logins'" @click="moove('logins')" icon="key" label="Identifiant et Mot de passe" />
-              <b-menu-item v-else label="Identifiant et Mot de passe" disabled/>
-              <b-menu-item v-if="path !== 'avatar'" @click="moove('avatar')" icon="account" label="Modification Avatar" />
+              <b-menu-item v-if="path !== 'logins'" icon="key" label="Identifiant et Mot de passe" @click="moove('logins')" />
+              <b-menu-item v-else label="Identifiant et Mot de passe" disabled />
+              <b-menu-item v-if="path !== 'avatar'" icon="account" label="Modification Avatar" @click="moove('avatar')" />
               <b-menu-item v-else icon="account" label="Modification Avatar" disabled />
-              <b-menu-item v-if="path !== 'delete'" @click="moove('delete')" icon="delete" label="Suppression Compte" />
-              <b-menu-item v-else icon="delete" label="Suppression Compte" disabled/>
+              <b-menu-item v-if="path !== 'delete'" icon="delete" label="Suppression Compte" @click="moove('delete')" />
+              <b-menu-item v-else icon="delete" label="Suppression Compte" disabled />
             </b-menu-list>
           </b-menu>
         </div>
-        <div class="column is-4 is-offset-3 box has-background-grey-lighter" id="context">
-          <div v-if="path === 'default' " ><AccountInfos /></div>
-          <div v-if="path === 'logins' " ><AccountLogins /></div>
-        </div><br />
+        <div id="context" class="column is-4 is-offset-3 box has-background-grey-lighter">
+          <div v-if="path === 'default' ">
+            <AccountInfos />
+          </div>
+          <div v-if="path === 'logins' ">
+            <AccountLogins />
+          </div>
+        </div><br>
       </div>
     </div>
   </section>
--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,10 @@
       <b-field grouped>
         <b-field label="Rechercher" expanded>
           <b-dropdown
-            @input="sortArray"
             v-model="selectedOptions"
             multiple
             aria-role="list"
+            @input="sortArray"
           >
             <template #trigger>
               <b-button
@@ -34,8 +34,148 @@
             </b-dropdown-item>
           </b-dropdown>
           <b-input v-model="search" type="text" placeholder="Rechercher..." expanded />
+          <b-button
+            type="is-primary"
+            icon-right="filter"
+            @click="filterHide = !filterHide"
+          >
+            Filtre
+          </b-button>
         </b-field>
       </b-field>
+      <div v-if="!filterHide" id="cardFilter" style="border: solid 1px lightgrey" class="p-2">
+        <div class="is-flex is-justify-content-center">
+          <b-dropdown
+            v-model="saisonFilter"
+            multiple
+            aria-role="list"
+          >
+            <template #trigger>
+              <b-button
+                type="is-primary"
+                icon-right="menu-down"
+              >
+                Saisons ({{ saisonFilter.length }})
+              </b-button>
+            </template>
+
+            <b-dropdown-item value="toutes" aria-role="listitem">
+              <span>Toutes</span>
+            </b-dropdown-item>
+
+            <b-dropdown-item value="printemps" aria-role="listitem">
+              <span>Printemps</span>
+            </b-dropdown-item>
+
+            <b-dropdown-item value="été" aria-role="listitem">
+              <span>Été</span>
+            </b-dropdown-item>
+
+            <b-dropdown-item value="automne" aria-role="listitem">
+              <span>Automne</span>
+            </b-dropdown-item>
+
+            <b-dropdown-item value="hiver" aria-role="listitem">
+              <span>Hiver</span>
+            </b-dropdown-item>
+          </b-dropdown>
+
+          <b-dropdown
+            v-model="difficulteFilter"
+            multiple
+            aria-role="list"
+          >
+            <template #trigger>
+              <b-button
+                type="is-primary"
+                icon-right="menu-down"
+              >
+                Difficulté ({{ difficulteFilter.length }})
+              </b-button>
+            </template>
+
+            <b-dropdown-item value="1" aria-role="listitem">
+              <span>Facile</span>
+            </b-dropdown-item>
+
+            <b-dropdown-item value="2" aria-role="listitem">
+              <span>Normale</span>
+            </b-dropdown-item>
+
+            <b-dropdown-item value="3" aria-role="listitem">
+              <span>Difficile</span>
+            </b-dropdown-item>
+          </b-dropdown>
+
+          <b-dropdown
+            v-model="regimeFilter"
+            multiple
+            aria-role="list"
+          >
+            <template #trigger>
+              <b-button
+                type="is-primary"
+                icon-right="menu-down"
+              >
+                Régime ({{ regimeFilter.length }})
+              </b-button>
+            </template>
+
+            <b-dropdown-item value="sans gluten" aria-role="listitem">
+              <span>Sans gluten</span>
+            </b-dropdown-item>
+
+            <b-dropdown-item value="végétarien" aria-role="listitem">
+              <span>Végétarien</span>
+            </b-dropdown-item>
+
+            <b-dropdown-item value="vegan" aria-role="listitem">
+              <span>Vegan</span>
+            </b-dropdown-item>
+
+            <b-dropdown-item value="flexitarien" aria-role="listitem">
+              <span>Flexitarien</span>
+            </b-dropdown-item>
+
+            <b-dropdown-item value="hypocalorique" aria-role="listitem">
+              <span>Hypocalorique</span>
+            </b-dropdown-item>
+
+            <b-dropdown-item value="carnivore" aria-role="listitem">
+              <span>Carnivore</span>
+            </b-dropdown-item>
+
+            <b-dropdown-item value="omnivore" aria-role="listitem">
+              <span>Omnivore</span>
+            </b-dropdown-item>
+          </b-dropdown>
+
+          <b-dropdown
+            v-model="produitFilter"
+            multiple
+            aria-role="list"
+          >
+            <template #trigger>
+              <b-button
+                type="is-primary"
+                icon-right="menu-down"
+              >
+                Produit ({{ produitFilter.length }})
+              </b-button>
+            </template>
+
+            <b-dropdown-item v-for="produit in produits" :key="produit.id" :value="produit.id" aria-role="listitem">
+              <span>{{ produit.nom }}</span>
+            </b-dropdown-item>
+          </b-dropdown>
+        </div>
+
+        <div style="width:100%;" class="is-flex is-justify-content-center mt-2">
+          <button class="button" @click="filtrer()">
+            Filtrer
+          </button>
+        </div>
+      </div>
     </div>
     <div class="is-flex">
       <b-button v-if="$auth.loggedIn" type="is-primary" label="Créer une recette" @click="$router.push('/recette/creation')" />
@@ -46,7 +186,7 @@
     <div v-else class="columns is-multiline is-justify-content-center">
       <b-loading v-model="loading" :is-full-page="false" />
       <CardRecipe
-        v-for="recipe in filteredRecipes"
+        v-for="recipe in searchRecipes"
         :id="recipe.id"
         :key="recipe.id"
         :difficulty="recipe.difficulte"
@@ -71,15 +211,22 @@ export default {
     return {
       error: false,
       loading: true,
+      filterHide: true,
       recipes: [],
+      filteredRecipes: [],
       search: '',
       selectedOptions: [],
-      idUserSuivis: []
+      idUserSuivis: [],
+      saisonFilter: ['toutes', 'printemps', 'été', 'automne', 'hiver'],
+      difficulteFilter: ['1', '2', '3'],
+      regimeFilter: ['sans gluten', 'végétarien', 'vegan', 'flexitarien', 'hypocalorique', 'carnivore', 'omnivore'],
+      produits: [],
+      produitFilter: []
     }
   },
   computed: {
-    filteredRecipes () {
-      return this.recipes.filter((recipe) => {
+    searchRecipes () {
+      return this.filteredRecipes.filter((recipe) => {
         return recipe.titre.toLowerCase().includes(this.search.toLowerCase())
       })
     }
@@ -89,6 +236,7 @@ export default {
       .get('/api/recettes')
       .then((response) => {
         this.recipes = response.data
+        this.filteredRecipes = response.data
         this.error = false
       })
       .catch(() => {
@@ -97,12 +245,25 @@ export default {
       .finally(() => {
         this.loading = false
       })
+
+    this.$axios
+      .get('/api/produits')
+      .then((response) => {
+        this.produits = response.data
+        this.produits.forEach((produit) => {
+          this.produitFilter.push(produit.id)
+        })
+        this.error = false
+      })
+      .catch(() => {
+        this.error = true
+      })
   },
   methods: {
     sortArray () {
       for (let i = 0; i < this.selectedOptions.length; i++) {
         if (this.selectedOptions[i] === 'alpha') {
-          this.recipes.sort((a, b) => {
+          this.filteredRecipes.sort((a, b) => {
             if (a.titre.toLowerCase() < b.titre.toLowerCase()) {
               return -1
             }
@@ -113,7 +274,7 @@ export default {
           })
         }
         if (this.selectedOptions[i] === 'difficulte') {
-          this.recipes.sort((a, b) => {
+          this.filteredRecipes.sort((a, b) => {
             if (a.difficulte < b.difficulte) {
               return -1
             }
@@ -124,7 +285,7 @@ export default {
           })
         }
         if (this.selectedOptions[i] === 'tpsrea') {
-          this.recipes.sort((a, b) => {
+          this.filteredRecipes.sort((a, b) => {
             if (a.temps < b.temps) {
               return -1
             }
@@ -140,12 +301,46 @@ export default {
               response.data.forEach((user) => {
                 this.idUserSuivis.push(user.id)
               })
-              this.recipes = this.recipes.filter((recipe) => {
+              this.filteredRecipes = this.filteredRecipes.filter((recipe) => {
                 return this.idUserSuivis.includes(recipe.id_createur)
               })
             })
         }
       }
+    },
+
+    async filtrer () {
+      let arrayFilter = this.recipes
+
+      arrayFilter = arrayFilter.filter((recipe) => { return this.saisonFilter.includes(recipe.saison) })
+      arrayFilter = arrayFilter.filter((recipe) => { return this.difficulteFilter.includes((recipe.difficulte).toString()) })
+      arrayFilter = arrayFilter.filter((recipe) => { return this.regimeFilter.includes(recipe.regime) })
+
+      const asyncFilter = async (arr, predicate) => {
+        const results = await Promise.all(arr.map(predicate))
+        return arr.filter((_v, index) => results[index])
+      }
+
+      arrayFilter = await asyncFilter(arrayFilter, (recipe) => {
+        return this.productInRecipe(recipe)
+      })
+      this.filteredRecipes = arrayFilter
+    },
+
+    async productInRecipe (recipe) {
+      const produitsRecetteArray = []
+      await this.$axios
+        .get('/api/recette/produits/' + recipe.id)
+        .then((response) => {
+          const produitsRecette = response.data
+          produitsRecette.forEach((produit) => {
+            produitsRecetteArray.push(produit.id_produit)
+          })
+        })
+      const res = produitsRecetteArray.every((element) => {
+        return this.produitFilter.includes(element)
+      })
+      return res
     }
   }
 }
