c510212d0cff4f79c60c0ccb862686f6fcc65bfe
Calvin Lambert <calvin.lambert@protonmail.com>
2022-02-11 15:08:27 +0100

    :sparkles: Add axios request for step save

--- pages/recette/creation/index.vue
+++ pages/recette/creation/index.vue
@@ -97,12 +97,17 @@
         Liste des ingrédients :
       </h5>
       <div class="columns is-multiline">
-        <b-input v-model="listing[0]" class="mt-2 column is-one-quarter is-full-mobile is-full-tablet" />
-        <div v-for="n in parseInt(nbing-1)" :key="n" class="mt-2 column is-one-quarter is-full-mobile is-full-tablet">
-          <b-input v-model="listing[n]" />
-        </div>
+        <b-input v-model="listing[0].nom" class="mt-2 column is-one-third" placeholder="Nom de l'ingrédient" />
+        <b-input v-model="listing[0].poid" class="mt-2 column is-one-third" placeholder="Poid de l'ingrédient" />
+        <b-input v-model="listing[0].unite" class="mt-2 column is-one-third" placeholder="Unité volumétrique" />
+      </div>
+      <div v-for="n in parseInt(nbing-1)" :key="n" class="mt-2 columns">
+        <b-input v-model="listing[n].nom" class="mt-2 column is-one-third" placeholder="Nom de l'ingrédient" />
+        <b-input v-model="listing[n].poid" class="mt-2 column is-one-third" placeholder="Poid de l'ingrédient" />
+        <b-input v-model="listing[n].unite" class="mt-2 column is-one-third" placeholder="Unité volumétrique" />
       </div>
 
+      <!--Ustencile front-->
       <hr>
       <b-field label="Nombres d'ustensiles">
         <b-numberinput v-model="nbust" placeholder="5" min="1" max="5" />
@@ -111,12 +116,14 @@
         Liste des ustensiles :
       </h5>
       <div class="columns is-multiline">
-        <b-input v-model="listust[0]" class="mt-2 column is-one-quarter is-full-mobile is-full-tablet" />
+        <b-input v-model="listust[0]" class="mt-2 column is-one-quarter is-full-mobile is-full-tablet" placeholder="Nom ingrédient" />
         <div v-for="n in parseInt(nbust-1)" :key="n" class="mt-2 column is-one-quarter is-full-mobile is-full-tablet">
           <b-input v-model="listust[n]" />
         </div>
       </div>
+      {{ listust }}
 
+      <!-- Etape front -->
       <hr>
       <b-field label="Nombres d'étapes de la recette">
         <b-numberinput v-model="nbetapes" placeholder="5" min="1" max="15" />
@@ -124,19 +131,33 @@
       <h5 class="title is-5">
         Etape : 1
       </h5>
+      <h6 class="subtitle is-6">
+        Titre de l'étape ( facultatif )
+      </h6>
+      <b-input v-model="descetape[0].titre" placeholder="Titre de votre étape"/>
+      <h6 class="subtitle is-6">
+        Description
+      </h6>
       <b-field>
-        <b-input v-model="descetape[0].desc" type="textarea" />
+        <b-input v-model="descetape[0].desc" type="textarea" placeholder="Description de l'étape"/>
       </b-field>
       <h6 class="subtitle is-6">
         Temps de l'étape :
       </h6>
-      <b-input v-model="descetape[0].temps" />
+      <b-input v-model="descetape[0].temps" placeholder="Temps requis pour l'étape"/>
 
       <div v-for="n in parseInt(nbetapes-1)" :key="n" class="mt-3">
         <hr>
         <h5 class="title is-5">
           Etape : {{ n + 1 }}
         </h5>
+        <h6 class="subtitle is-6">
+          Titre de l'étape ( facultatif )
+        </h6>
+        <b-input v-model="descetape[n].titre" />
+        <h6 class="subtitle is-6">
+          Description
+        </h6>
         <b-field>
           <b-input v-model="descetape[n].desc" type="textarea" />
         </b-field>
@@ -145,8 +166,9 @@
         </h6>
         <b-input v-model="descetape[n].temps" />
       </div>
-      {{ descetape }}
     </section>
+
+    <!--Button envoie-->
     <b-button expanded class="button is-primary" @click="sendRecette">
       Envoyer la recette
     </b-button>
@@ -171,82 +193,158 @@ export default {
       nbust: 1,
       descetape: [
         {
-          num: 1,
           desc: '',
+          titre: '',
           temps: 0
         },
         {
-          num: 2,
           desc: '',
+          titre: '',
           temps: 0
         },
         {
-          num: 3,
           desc: '',
+          titre: '',
           temps: 0
         },
         {
-          num: 4,
           desc: '',
+          titre: '',
           temps: 0
         },
         {
-          num: 5,
           desc: '',
+          titre: '',
           temps: 0
         },
         {
-          num: 6,
           desc: '',
+          titre: '',
           temps: 0
         },
         {
-          num: 7,
           desc: '',
+          titre: '',
           temps: 0
         },
         {
-          num: 8,
           desc: '',
+          titre: '',
           temps: 0
         },
         {
-          num: 9,
           desc: '',
+          titre: '',
           temps: 0
         },
         {
-          num: 10,
           desc: '',
+          titre: '',
           temps: 0
         },
         {
-          num: 11,
           desc: '',
+          titre: '',
           temps: 0
         },
         {
-          num: 12,
           desc: '',
+          titre: '',
           temps: 0
         },
         {
-          num: 13,
           desc: '',
+          titre: '',
           temps: 0
         },
         {
-          num: 14,
           desc: '',
+          titre: '',
           temps: 0
         },
         {
-          num: 15,
           desc: '',
+          titre: '',
           temps: 0
         }
       ],
-      listing: [],
+      listing: [
+        {
+          nom: '',
+          poid: 0,
+          unite: ''
+        },
+        {
+          nom: '',
+          poid: 0,
+          unite: ''
+        },
+        {
+          nom: '',
+          poid: 0,
+          unite: ''
+        },
+        {
+          nom: '',
+          poid: 0,
+          unite: ''
+        },
+        {
+          nom: '',
+          poid: 0,
+          unite: ''
+        },
+        {
+          nom: '',
+          poid: 0,
+          unite: ''
+        },
+        {
+          nom: '',
+          poid: 0,
+          unite: ''
+        },
+        {
+          nom: '',
+          poid: 0,
+          unite: ''
+        },
+        {
+          nom: '',
+          poid: 0,
+          unite: ''
+        },
+        {
+          nom: '',
+          poid: 0,
+          unite: ''
+        },
+        {
+          nom: '',
+          poid: 0,
+          unite: ''
+        },
+        {
+          nom: '',
+          poid: 0,
+          unite: ''
+        },
+        {
+          nom: '',
+          poid: 0,
+          unite: ''
+        },
+        {
+          nom: '',
+          poid: 0,
+          unite: ''
+        },
+        {
+          nom: '',
+          poid: 0,
+          unite: ''
+        }
+      ],
       listust: []
     }
   },
@@ -262,11 +360,18 @@ export default {
         nb_pers: this.nbpers,
         regime: this.regime
       }).then((response) => {
-        console.log(response)
         const idrecette = response.data.id
         this.$axios.post('/api/auth/etape', {
           id_recette: idrecette,
           etapes: this.descetape
+        })
+        this.$axios.post('/api/auth/produitrecette', {
+          id_recette: idrecette,
+          produits: this.listing
+        })
+        this.$axios.post('/api/auth/ustensilerecette', {
+          id_recette: idrecette,
+          ustenciles: this.listust
         })
       })
     }
